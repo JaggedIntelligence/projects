f873ca94f350a3fa110eea935250ac7bfb4470e0
JaggedIntelligence <srview9@gmail.com>
2026-06-16 13:39:04 -0700

    uplot library based Charts

---
+++ showmeedge/app/(app)/plots/page.tsx
@@ -0,0 +1,41 @@
+"use client"
+
+import React from 'react';
+import  UplotReact  from 'uplot-react';
+import 'uplot/dist/uPlot.min.css';
+
+const data = [
+    [0, 1, 2, 3, 4, 5],
+    [0, 1, 2, 3, 4, 5],
+];
+
+const options = {
+    width: 400,
+    height: 300,
+    scales: {
+        x: {
+            time: false,
+            range: [-0.5, 5.5],
+        },
+        y: {},
+    },
+    axes: [{}],
+    series: [
+        {},
+        {
+            stroke: 'blue',
+        },
+    ],
+};
+
+const Chart = () => <UplotReact options={options} data={data} onCreate={(chart) => {}} onDelete={(chart) => {}} />;
+
+
+export default function MuPlotChartJSExample() {
+    return (
+        <div>
+            <Chart />
+        </div>
+    );
+}
+
